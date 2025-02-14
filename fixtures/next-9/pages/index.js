e05a1e862788bb73944285d82705fdb22d06dae5
Ethan Arrowood <ethan@arrowood.dev>
2025-02-14 11:36:15 -0700

    add static content for next-9 fixture

--- fixtures/next-9/pages/index.js
+++ fixtures/next-9/pages/index.js
@@ -4,6 +4,7 @@ export default function Index() {
 	return (
 		<div>
 			<h1>Next.js v9</h1>
+			<img src='/static/hdb-logo.png' alt='HarperDB Logo' />
 			<Link href="/page-2">Page 2</Link>
 		</div>
 	);
